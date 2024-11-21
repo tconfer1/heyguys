a080489097291e9db7a20e8f9f47805b172d380d
tconfer1 <taylorconfer2002@gmail.com>
2024-11-21 11:33:06 -0500

    Test

--- Module 10-3.py
+++ Module 10-3.py
@@ -1,1 +1,7 @@
-
+# Taylor Confer 
+# assigning values to two variables, adding them together into a third variable, and outputting the result
+x = 20 
+y = 15 
+z = 35 
+if x + y == 35: 
+    print ("the sum of x and y is z") 
